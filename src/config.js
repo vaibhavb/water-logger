4c15996eaaa7d2a9f433f2151a5ce417ab5f27b0
Vaibhav Bhandari <vaibhavb@gmail.com>
2017-09-20 12:43:06 -0700

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  clientId: '',
+  clientId: '446144537611-pskmkt54i7jrh0ajf7gv5lv4vsklpjm8.apps.googleusercontent.com',
   scope: 'https://www.googleapis.com/auth/spreadsheets',
-  spreadsheetId: ''
+  spreadsheetId: '1OwMH_Z4ybcLIb3VwnYmT_sdHkNrUEeLDVVJdRzpT5NA'
 }
