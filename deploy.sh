7516b2cb8957e5c9273ce5b7b6001a97abb9c7e2
Vaibhav Bhandari <vaibhavb@gmail.com>
2017-09-20 02:19:41 -0700

    delete

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git stash save
-git checkout -B gh-pages
-git add -f build
-git commit -am "Rebuild website"
-git filter-branch -f --prune-empty --subdirectory-filter build
-git push -f origin gh-pages
-git checkout -
-git stash pop
--- src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  clientId: '446144537611-pskmkt54i7jrh0ajf7gv5lv4vsklpjm8.apps.googleusercontent.com',
-  scope: 'https://www.googleapis.com/auth/spreadsheets',
-  spreadsheetId: '1OwMH_Z4ybcLIb3VwnYmT_sdHkNrUEeLDVVJdRzpT5NA'
-}
